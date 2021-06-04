9e0340fdf798a63756656edb384dee3154a8f39e
lucasvittal2 <lucasvital@tecgraf.puc-rio.br>
2021-06-04 07:58:19 -0300

    initial prototype

--- App.tsx
+++ App.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading'
+import { ThemeProvider} from 'styled-components';
+import {
+ useFonts,
+ Poppins_400Regular,
+ Poppins_500Medium,
+ Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+import { Righteous_400Regular } from '@expo-google-fonts/righteous'
+
+import theme from './src/global/styles/theme';
+import {DashBoard} from './src/screens/Dashboard/index'
+import { Text } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Righteous_400Regular
+  });
+  if(! fontsLoaded){
+    return <AppLoading/>;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ThemeProvider theme = { theme }>
+      <DashBoard/>
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
